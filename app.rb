98563e02195ac1f5ec132ea376811402400cf6d2
Amy Jordan <amy_jordan@live.co.uk>
2019-03-21 09:49:00 +0000

    First commit - welcome page says Hello there!

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class Birthday < Sinatra::Base
+
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  run! if app_file == $0
+end
---
+++ spec/features/welcome_page_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../app.rb'
+
+feature 'welcome page' do
+  scenario 'says Hello there!' do
+    visit('/')
+    expect(page).to have_content 'Hello there!'
+  end
+end

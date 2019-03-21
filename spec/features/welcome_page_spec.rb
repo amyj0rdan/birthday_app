455079e9da05c53039fb6fb327da5c23d5b1d382
Amy Jordan <amy_jordan@live.co.uk>
2019-03-21 10:48:34 +0000

    User can type in name

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -8,6 +8,16 @@ feature 'welcome page' do
 
   scenario "asks What's your name?" do
     visit('/')
-    expect(page).to have_content "What's your birthday?"
+    expect(page).to have_content "What's your name?"
+  end
+
+  scenario 'users fills in birthday' do
+    visit('/')
+    page.fill_in 'name', with: 'Amy'
+  end
+
+  scenario "asks When's your birthday?" do
+    visit('/')
+    expect(page).to have_content "When's your birthday?"
   end
 end
